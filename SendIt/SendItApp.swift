1ffb0a6c3c3bb70559bfbc20a12b0527d8f5db73
Wojciech Kozioł <wojtek.koziol.pl@gmail.com>
2024-10-24 17:47:33 +0200

    Initial Commit

---
+++ SendIt/SendItApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendItApp.swift
+//  SendIt
+//
+//  Created by Wojciech Kozioł on 24/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SendItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
